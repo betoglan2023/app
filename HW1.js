85411df333d5b0a4b192e494766085f6487931ca
test050505 <testtesterrmqwe098@gmail.com>
2023-10-11 20:02:43 +0500

    HW

--- HW1.js
+++ HW1.js
@@ -1,28 +1,44 @@
-let san = 10
+//let san = 10
 
-let uytgeyansan = "25"
+//let uytgeyansan = "25"
 
-let strToNum = parseInt (uytgeyansan,10)
+//let strToNum = parseInt (uytgeyansan,10)
 
-console.log (parseInt (uytgeyansan,10) * san)
+//console.log (parseInt (uytgeyansan,10) * san)
 
 
 
 
 // 2nji funksiya
 
-function atuytgeyan  (at,booleanValue){
+//function atuytgeyan  (at,booleanValue){
 
-if (booleanValue){
-    return "Umyt"
-}   
-    return "Berdi"
-}
+//if (booleanValue){
+//    return "Umyt"
+//}   
+//    return "Berdi"
+//}
 
-const netije = atuytgeyan("Umyt",true)
-console.log(netije)
+//const netije = atuytgeyan("Umyt",true)
+//console.log(netije)
 
 // Homework
 // create a function that takes 2 arguments as (number, string)
 // const netije = exampleFunction(5, "berdi")
 // console.log(netije) output should be: "berdiberdiberdiberdi" as you can see 5 times
+
+
+//basdaky edenim
+
+//const netije1 =  ("berdi".repeat(5))
+//console.log (netije1)
+
+
+function exercise (number, string){
+let number = 5
+let string = "berdi"
+let sum = nummber*string
+return sum
+}
+const netije = exercise (5,"berdi")
+console.log (netije)
---
+++ HW2.js
@@ -0,0 +1,16 @@
+
+
+function exercise (san, text){
+
+let number = 5
+
+let string = "berdi"
+
+let sum = number*string
+
+return sum
+}
+
+const netije = exercise (5,"berdi")
+
+console.log (netije)
