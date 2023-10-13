33b8948467a92c169329df98b5a05814063f8f58
test050505 <testtesterrmqwe098@gmail.com>
2023-10-13 14:15:29 +0500

    new

---
+++ Lesson-2/HW3.js
@@ -0,0 +1,17 @@
+// Homework for learning slice, substr methods
+
+// 1) arrow function, parameters(num, text)
+// const netije = myfunc(6, "meselemdir")
+// console.log(netije) output: dirmeselem,
+// const netije2 = myFunc(10, "meselem")
+// output: senin beren sanyn beren sozun harpydan uly
+
+
+
+function example (num,text){
+
+let hat = "meselemdir";
+return hat.substring(7);
+}
+
+console.log (example(5))
