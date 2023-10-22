5a658ba72772947664c67370b7bef2798b7f5cee
test050505 <testtesterrmqwe098@gmail.com>
2023-10-22 05:39:10 +0500

    last11

--- Lesson-3/HW2.js
+++ Lesson-3/HW2.js
@@ -4,30 +4,27 @@
 // 2) text = "salam"
 // etmeli zat s-1, a-2, l-3, a-4, m-5
 
-let sanlar = [];
+// let sanlar = [];
 
-const myFunc = (num) => {
-  for (i = 0; i < num; i++) {
-    sanlar.push(i);
-  }
-};
-myFunc(55);
-console.log(sanlar);
-
-// let arr = [];
-// let str = "";
-// let str1 = "";
-// let str2 = "";
-// const myFunc = (text) => {
-//   for (i = 0; i < text.length; i++) {
-//     str = text[0 + i];
-//     str1 = str + "-" + i;
-//     console.log(str1);
+// const myFunc = (num) => {
+//   for (i = 0; i < num; i++) {
+//     sanlar.push(i);
 //   }
 // };
-// myFunc("salam");
+// myFunc(55);
+// console.log(sanlar);
 
-// 2nji netijede s-0 a-1 l-3 ....... yalak cykya sanalry sonam hersi ayry setirde
-//  output tocno senki yaly bolsunmy ?
-//  howa meñki yaly eday
-//
+let arr = [];
+let str = "";
+let str1 = "";
+let str2 = "";
+const myFunc = (text) => {
+  for (i = 1; i < text.length + 1; i++) {
+    str = text[-1 + i];
+    str1 += str + "-" + i + ", ";
+  }
+};
+myFunc("Welcome");
+console.log(str1);
+
+// str1 = str + "-" + i;
