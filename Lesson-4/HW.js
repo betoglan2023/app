4875ac31fcdf9c006570d138e3d5a90318ea57f4
berdi00 <berdinazarov731@gmail.com>
2023-10-24 20:34:51 +0500

    add hw

---
+++ Lesson-4/HW.js
@@ -0,0 +1,75 @@
+// nested object düşindirmandirn
+const nestedObj = {
+  at: "Umyt",
+  caga: {
+    at: "Meret",
+  },
+};
+console.log(nestedObj.caga.at); // returns Meret
+
+// at we age value-lary uytgetmeli for ulanyp
+// at-a berdi, age-a 23 goymaly
+const person = {
+  at: "Umyt",
+  age: 24,
+  student: true,
+  job: " frontend developer",
+};
+
+// HW Her bankyn oz bahasyny ay boyuncha goshmaly
+// in sonunda: Halkbank account: ... man
+//             Merkezi Bank: ... man
+//             Dayhan Bank: ... man
+
+const bankAccounts = {
+  january: {
+    HalkBank: 232,
+    MerkeziBank: 133,
+    DayhanBank: 252,
+  },
+  february: {
+    HalkBank: 563,
+    MerkeziBank: 333,
+    DayhanBank: 222,
+  },
+  march: {
+    HalkBank: 323,
+    MerkeziBank: 373,
+    DayhanBank: 226,
+  },
+  april: {
+    HalkBank: 263,
+    MerkeziBank: 33,
+    DayhanBank: 22,
+  },
+  may: {
+    HalkBank: 213,
+    MerkeziBank: 303,
+    DayhanBank: 272,
+  },
+  june: {
+    HalkBank: 223,
+    MerkeziBank: 343,
+    DayhanBank: 202,
+  },
+  july: {
+    HalkBank: 223,
+    MerkeziBank: 333,
+    DayhanBank: 272,
+  },
+  august: {
+    HalkBank: 223,
+    MerkeziBank: 323,
+    DayhanBank: 232,
+  },
+  september: {
+    HalkBank: 213,
+    MerkeziBank: 323,
+    DayhanBank: 212,
+  },
+  june: {
+    HalkBank: 273,
+    MerkeziBank: 303,
+    DayhanBank: 282,
+  },
+};
